7da6fc2021fa94e2bec466997448152a60941236
brayomwas <brayomwas95@gmail.com>
2020-09-12 08:38:04 +0300

    Initial Commit

---
+++ src/components/Hero.js
@@ -0,0 +1,33 @@
+import React,{ useEffect } from "react"
+import gsap from 'gsap'
+import Heading from "./Heading"
+import AgeHeading from "./AgeHeading"
+import BirthdayGirl from "../assets/birthday-girl.svg"
+
+export default function Hero() {
+    useEffect(() => {
+        const tl = gsap.timeline();
+        tl.from('#balloon-with-string', {duration: 2, x: 100, y: 100})
+          .fromTo('#balloon', {x: -4.5, y: -4.5}, {duration: 2, x: 4.5, y: 4.5, fill: '#82b1ff', yoyo: true,repeat: -1})
+    })
+  return (
+    <div className="section">
+      <div className="row">
+        <div className="col s7 valign-wrapper">
+          <div className="headings">
+            <Heading
+              classes="typewriter light-green-text text-accent-3"
+              text="Happy Birthday Kym"
+            />
+            <AgeHeading classes="typewriter age light-green-text text-accent-3" text="Happy 24th" />
+          </div>
+        </div>
+        <div className="col s5">
+          <div className="present-svg">
+            <BirthdayGirl />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
-
+import 'materialize-css/dist/css/materialize.min.css'
+import Hero from '../components/Hero'
+import Cake from '../components/Cake'
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div >
+    
+      <Hero />
+      <Cake />
+  </div>
+  )
+
 }
